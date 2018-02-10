17508b29660e1ac7c1a6aef4bb72f03dc7b70400
MasaKusar <masa.kusar@gmail.com>
2018-02-10 19:01:12 +0100

    menjava mape

--- server.R
+++ server.R
@@ -1,15 +1,43 @@
 library(shiny)
 library(digest) # digest() Create hash function digests for R objects
 
-formName <- "januar"
+formName <- "februar"
 resultsDir <- file.path("./data", formName)
 dir.create(resultsDir, recursive = TRUE, showWarnings = FALSE)
 
 # names of the fields on the form we want to save
 fieldNames <- c("ime",
-                "datum1",
-                "datum2",
-                "vrsta"
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "11",
+                "12",
+                "13",
+                "14",
+                "15",
+                "16",
+                "17",
+                "18",
+                "19",
+                "20",
+                "21",
+                "22",
+                "23",
+                "24",
+                "25",
+                "26",
+                "27",
+                "28"#,
+                #"29",
+                #"30",
+                #"31"
 )
 
 shinyServer(function(input, output, session) {
@@ -23,15 +51,10 @@ shinyServer(function(input, output, session) {
    #   session$sendCustomMessage(type = "enableBtn", list(id = "submitBtn"))
   #  }
   #})
-  
-  observe({
-    date <- as.Date(input$datum1)
-    updateDateInput(session, "datum2", value = date)
-  })
-  
+ 
   # the name to show in the Thank you confirmation page
   output$thanksName <- renderText({
-    paste0("Thank you, ", input$ime, "!")
+    paste0("Hvala, dr. ", input$ime, "!")
   })
   
   # we need to have a quasi-variable flag to indicate when the form was submitted
@@ -45,7 +68,8 @@ shinyServer(function(input, output, session) {
   # submit the form  
   observe({
     #if (input$submitConfirmDlg < 1) return(NULL)
-    if (input$submitBtn < 1) return(NULL)
+    if (input$submitBtn < 1) 
+      return(NULL)
     
     # read the info into a dataframe
     isolate(
@@ -56,9 +80,7 @@ shinyServer(function(input, output, session) {
     isolate(
       fileName <- paste0(
         paste(
-          getFormattedTimestamp(),
-          input$ime,
-          sep = "_"
+          input$ime
         ),
         ".csv"
       )
